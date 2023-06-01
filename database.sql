5ee6030cf027428c5a8ea43ad04528b9eae50cb6
Tata Pestova <tata.pestova@inbox.ru>
2023-06-01 21:59:28 +0300

    after 1 check

--- database.sql
+++ database.sql
@@ -1,10 +1,12 @@
+DROP TABLE IF EXISTS urls;
+
 CREATE TABLE urls (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
   name varchar(255) UNIQUE NOT NULL,
   created_at timestamp NOT NULL
 );
 
-
+DROP TABLE IF EXISTS url_checks;
 
 CREATE TABLE url_checks (
   id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
